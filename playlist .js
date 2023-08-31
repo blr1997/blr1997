d079c8b631f58f155bcb895c979c1768259c2aab
slice-rajesh <rajesh.kumar@sliceit.com>
2023-08-31 23:09:04 +0530

    added comments for Code readability

--- playlist .js
+++ playlist .js
@@ -1,20 +1,24 @@
 class RecentlyPlayedStore {
   constructor(storage) {
     this.storage = storage;
+// Using a Map to store user-song pairs
     this.store = new Map();
   }
 
   playSong(user, song) {
+ // If the user already exists, remove the old occurrence users
     if (this.store.has(user)) {
       const songs = this.store.get(user);
       songs.splice(songs.indexOf(song), 1);
     }
 
+ // Add the song at the beginning of the list for the users
     if (!this.store.has(user)) {
       this.store.set(user, []);
     }
     this.store.get(user).unshift(song);
 
+ // If the store becomes full, user remove the least recently played song from storage list
     if (this.store.get(user).length > this.storage) {
       this.store.get(user).pop();
     }
@@ -29,6 +33,7 @@ class RecentlyPlayedStore {
   }
 }
 
+// Example usage
 const store = new RecentlyPlayedStore(3);
 
 store.playSong("User1", "S1");
