c21eedb3bf06c73520f451573c2fceba9d84d032
slice-rajesh <rajesh.kumar@sliceit.com>
2023-08-31 23:24:53 +0530

    added try & catch block to handle the error

--- playlist .js
+++ playlist .js
@@ -1,51 +1,56 @@
 class RecentlyPlayedStore {
   constructor(storage) {
     this.storage = storage;
-// Using a Map to store user-song pairs
-    this.store = new Map();
+    this.store = new Map(); // Using a Map to store user-song pairs
   }
-
   playSong(user, song) {
- // If the user already exists, remove the old occurrence users
-    if (this.store.has(user)) {
-      const songs = this.store.get(user);
-      songs.splice(songs.indexOf(song), 1);
-    }
+    // If the user already exists, remove the old occurrence user
+    try {
+      if (this.store.has(user)) {
+        const songs = this.store.get(user);
+        songs.splice(songs.indexOf(song), 1);
+      }
 
- // Add the song at the beginning of the list for the users
-    if (!this.store.has(user)) {
-      this.store.set(user, []);
-    }
-    this.store.get(user).unshift(song);
+      // Add the song at the beginning of the list for the users
+      if (!this.store.has(user)) {
+        this.store.set(user, []);
+      }
+      this.store.get(user).unshift(song);
 
- // If the store becomes full, user remove the least recently played song from storage list
-    if (this.store.get(user).length > this.storage) {
-      this.store.get(user).pop();
+      // If the store becomes full, user remove the least recently played song
+      if (this.store.get(user).length > this.storage) {
+        this.store.get(user).pop();
+      }
+    } catch (err) {
+      throw err;
     }
   }
 
   getRecentlyPlayed(user) {
-    if (this.store.has(user)) {
-      return this.store.get(user);
-    } else {
-      return [];
+    try {
+      if (this.store.has(user)) {
+        return this.store.get(user);
+      } else {
+        return [];
+      }
+    } catch (err) {
+      throw err;
     }
   }
 }
 
 // Example usage
 const store = new RecentlyPlayedStore(3);
+store.playSong("user1", "S1");
+store.playSong("user1", "S2");
+store.playSong("user1", "S3");
+console.log(store.getRecentlyPlayed("user1"));
 
-store.playSong("User1", "S1");
-store.playSong("User1", "S2");
-store.playSong("User1", "S3");
-console.log(store.getRecentlyPlayed("User1")); 
+store.playSong("user1", "S4");
+console.log(store.getRecentlyPlayed("user1"));
 
-store.playSong("User1", "S4");
-console.log(store.getRecentlyPlayed("User1")); 
+store.playSong("user1", "S2");
+console.log(store.getRecentlyPlayed("user1"));
 
-store.playSong("User1", "S2");
-console.log(store.getRecentlyPlayed("User1"));
-
-store.playSong("User1", "S1");
-console.log(store.getRecentlyPlayed("User1"));
+store.playSong("user1", "S1");
+console.log(store.getRecentlyPlayed("user1"));
