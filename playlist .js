c5db941fef2bd106a97581484a3350611da4cbde
blr1997 <143738497+blr1997@users.noreply.github.com>
2023-08-31 22:23:50 +0530

    Add files via upload
    TestVagrant Assignment

---
+++ playlist .js
@@ -0,0 +1,46 @@
+class RecentlyPlayedStore {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.store = new Map();
+  }
+
+  playSong(user, song) {
+    if (this.store.has(user)) {
+      const songs = this.store.get(user);
+      songs.splice(songs.indexOf(song), 1);
+    }
+
+    if (!this.store.has(user)) {
+      this.store.set(user, []);
+    }
+    this.store.get(user).unshift(song);
+
+    if (this.store.get(user).length > this.capacity) {
+      this.store.get(user).pop();
+    }
+  }
+
+  getRecentlyPlayed(user) {
+    if (this.store.has(user)) {
+      return this.store.get(user);
+    } else {
+      return [];
+    }
+  }
+}
+
+const store = new RecentlyPlayedStore(3);
+
+store.playSong("User1", "S1");
+store.playSong("User1", "S2");
+store.playSong("User1", "S3");
+console.log(store.getRecentlyPlayed("User1")); 
+
+store.playSong("User1", "S4");
+console.log(store.getRecentlyPlayed("User1")); 
+
+store.playSong("User1", "S2");
+console.log(store.getRecentlyPlayed("User1"));
+
+store.playSong("User1", "S1");
+console.log(store.getRecentlyPlayed("User1"));
